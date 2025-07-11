a005aeb2fe7b3ad859262e907b683e406bb222a4
BRAVO Diego <diego.bravo.contact@gmail.com>
2025-07-11 21:50:09 +0200

    Player and enemy interaction with damage backshot etc...

--- code/level.py
+++ code/level.py
@@ -20,6 +20,8 @@ class Level:
 
         # atatck sprite group
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         #sprite setup
         self.create_map()
@@ -52,7 +54,12 @@ class Level:
                         if style == 'grass':
                             # create a grass tile
                             random_grass_image = choice(graphics['grass'])
-                            Tile((x, y), [self.visible_sprites, self.obstacles_sprites], 'grass', random_grass_image)
+                            Tile(
+                                (x, y), 
+                                [self.visible_sprites, self.obstacles_sprites, self.attackable_sprites], 
+                                'grass', 
+                                random_grass_image
+                            )
                             
                         if style == 'object':
                             # create an object tile
@@ -71,18 +78,21 @@ class Level:
                                     self.create_magic,
                                 )
                             else:
-
                                 if col == '390': monster_name = 'bamboo'
                                 elif col == '391': monster_name = 'spirit'
                                 elif col == '392': monster_name = 'raccoon'
                                 else: monster_name = 'squid'
 
-                                Enemy(monster_name, (x, y), [self.visible_sprites], self.obstacles_sprites)
-
-        
+                                Enemy(
+                                    monster_name, 
+                                    (x, y), 
+                                    [self.visible_sprites, self.attackable_sprites], 
+                                    self.obstacles_sprites,
+                                    self.damage_player
+                                )
         
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
     
     def create_magic(self, style, strength, cost):
         print(f'Creating magic of style: {style}, strength: {strength}, cost: {cost}')
@@ -99,17 +109,37 @@ class Level:
         else:
             print('Unknown magic style')
 
-    
     def destroy_attack(self):
         if self.current_attack:
             self.current_attack.kill()
             self.current_attack = None
     
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack in self.attack_sprites:
+                # check for collision with attackable sprites
+                hit_sprites = pygame.sprite.spritecollide(attack, self.attackable_sprites, False)
+                if hit_sprites:
+                    for target in hit_sprites:
+                        if target.sprite_type == 'grass':
+                            target.kill()
+                        
+                        else:
+                            target.get_damage(self.player, attack.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health -= amount
+            self.player.vulnerable = False
+            self.player.hurt_time = pygame.time.get_ticks()
+            
+                         
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
